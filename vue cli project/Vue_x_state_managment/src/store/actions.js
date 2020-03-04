671c217e299ab1ab47ad0d095b15921036232238
Sumit Goyal <sgoyal@americanfirstfinance.com>
2020-03-04 14:18:12 -0600

    "2020-03-04 14:18:11.826926"

--- vue cli project/Vue_x_state_managment/src/store/actions.js
+++
@@ -1,8 +0,0 @@
-// to get some common storage code to use all over the application
-// >> we can write this code directly into the store.js too
-//  make sure each file as its own unique namespace
-
-
-export const updatedValue = (context,payload)=>{
-   // context.commit('updatevalue',payload)
-}
